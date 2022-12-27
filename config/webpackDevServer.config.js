2dbf69bde7370d5b578d38c4dfe0e92183bffc2d
MrigankKumar <mrigank.kb@gmail.com>
2022-12-27 14:44:24 +0530

    added historyApiFallback in WebPackDevServer

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -93,12 +93,12 @@ module.exports = function (proxy, allowedHost) {
     https: getHttpsConfig(),
     host,
     overlay: false,
-    // historyApiFallback: {
-    //   // Paths with dots should still use the history fallback.
-    //   // See https://github.com/facebook/create-react-app/issues/387.
-    //   disableDotRule: true,
-    //   index: paths.publicUrlOrPath,
-    // },
+    historyApiFallback: {
+      // Paths with dots should still use the history fallback.
+      // See https://github.com/facebook/create-react-app/issues/387.
+      disableDotRule: false,
+      index: '/index.html',
+    },
   //   devServer: {
   //     historyApiFallback: true,
   //     contentBase: './',
